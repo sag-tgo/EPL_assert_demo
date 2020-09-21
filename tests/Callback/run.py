56a60eb5cb070ee6d55b5e106d111b5a23b0508e
sag-tgo <56433746+sag-tgo@users.noreply.github.com>
2020-09-21 16:59:02 +0100

    Added Callback example.

---
+++ tests/Callback/run.py
@@ -0,0 +1,22 @@
+from pysys.constants import *
+from pysys.basetest import BaseTest
+from apama.correlator import CorrelatorHelper
+import os
+
+class PySysTest(BaseTest):
+	def execute(self):
+		corr = CorrelatorHelper(self, name='correlator')
+		corr.start(logfile='correlator.log')
+		corr.injectEPL('../../../src/AssertHelper.mon')
+		corr.injectEPL('../../../src/callback/Assert.mon')
+		tests = os.listdir(self.input)
+		tests.sort()
+		for test in tests:
+			if test.endswith('.mon'):
+				corr.injectEPL(test)
+				corr.flush()
+		corr.shutdown()
+
+	def validate(self):
+		self.assertLineCount('correlator.log', 'ERROR', condition='==0')
+		self.assertGrep('correlator.log', 'ERROR', contains=False)
